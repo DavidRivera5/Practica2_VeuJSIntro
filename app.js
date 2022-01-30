452564076f15a7c1a95c8bbad04ddabc49fb6b6b
DavidRivera5 <98625201+DavidRivera5@users.noreply.github.com>
2022-01-29 22:39:04 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,84 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejemplo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <h1>Ejemplo Javascrtipt </h1>
-         <div class="mb-3">
-            <h2>Agregar elemento:</h2>
-            <input type="text" id="addItem" class="form-control"> 
-            <button id="addItemButton" class="btn btn-success">Add</button>
-            <button id="removeItemButton" class="btn btn-danger">Del Last</button>
-            <button id="removeItem" class="btn btn-danger">Del Selected</button>
-         </div>
-        <div class="mb-3">
-            <button id="hideElements" class="btn btn-secondary"><<</button>
-        </div>
-        
-        <div class="mb-3" id="list">
-            <!--lista de elementos-->
-            <div class="card">
-                <div class="card-body">
-                  <ul id="listItems">
-                      <!--aqui se agregarn los elementos-->
-                  </ul>
-                </div>
-              </div>            
-         </div>
-        <script src="app.js"></script>
+//Ocultar o mostrar div que contiene la lista
+const hide = document.querySelector('#hideElements');
+const listDiv = document.querySelector('#list');
 
-    </div>
+//Constantes para agregar elementos a la lista
+const addItemInput = document.querySelector('#addItem');//Input
+const addItemButton = document.querySelector('button#addItemButton'); //Button
 
+//Eliminar último item
+const removeItemButton=document.querySelector('button#removeItemButton');
 
+//Eliminar elemento seleccionado
+const removeItem=document.querySelector('button#removeItem');
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+//Mostrar y ocultar lista
+hide.addEventListener('click',() => {
+    if (listDiv.style.display == 'none'){
+        listDiv.style.display = 'block';
+        hide.textContent = '<<';
+    } else {
+        listDiv.style.display = 'none';
+        hide.textContent = '>>';
+    }
+    });
 
-  
+    //Agregar elementos a la lista
+    addItemButton.addEventListener('click', () => {
+        let list = document.querySelector('ul');
+        let li = document.createElement('li');
+        li.textContent = addItemInput.value;
+        list.appendChild(li);
+        addItemInput.value="";
+    });
 
-</body>
-</html>
+    //Eliminar último elemento de la lista 
+    removeItemButton.addEventListener('click',()  => {
+        let list = document.querySelector('ul');
+        let li = document.querySelector('li:last-child');
+        list.removeChild(li);
+    });
+    
+    //Obtener index del elemento seleccionado
+    function findIndex(elem) {
+        var i, len = items.length;
+        for (i=0; i<len; i++) {
+            if (items [i]===elem) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //Obtener index of element in list
+    var list = document.getElementById("listItems"),
+        items = list.getElementsByTagName("li");
+    list.onclick = function(e) {
+        var event = e || window.event,
+        src = event.target || event.srcElement;
+        var myIndex = findIndex(src);
+
+        //Alert(myIndex);
+        index=myIndex;
+        console.log(myIndex);
+
+        //Cambiar color del elemento seleccionado
+        if (event.target.tagName === 'LI' ) {
+            list.querySelectorAll('li').forEach(el=>el.classList.remove('alert', 'alert-success')); // Se agregan clases Bootstrap para los elementos
+            items[index].classList.add('alert','alert-success');
+        }
+    };
+
+    //Eliminar elemento seleccionado
+    removeItem.addEventListener('click', () => {
+        let showDiv = document.getElementById('list');
+        let divAlert = document.createElement('div');
+        let label = document.createElement('label');
+        label.innerHTML= 'Se eliminó el elemento ' +index;
+        divAlert.classList.add('alert', 'alert-danger'); //Clase de la alerta
+       
+
+        items[index].parentNode.removeChild(items[index]);
+        divAlert.appendChild(label);
+        showDiv.appendChild(divAlert);
+    });
